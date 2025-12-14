1356ce05d9a8122f8bd479572470bb187fa584af
amsound <74129410+amsound@users.noreply.github.com>
2025-12-14 12:03:41 +0000

    Refactor media player: clean up and add unique ID
    Removed unused imports, service registrations, and DSP update methods. Added unique ID configuration option for media player.

--- custom_components/kef/media_player.py
+++ custom_components/kef/media_player.py
@@ -3,13 +3,9 @@
 from __future__ import annotations
 
 from datetime import timedelta
-from functools import partial
-import ipaddress
 import logging
 
 from .aiokef import AsyncKefSpeaker
-from .aiokef import DSP_OPTION_MAPPING
-from getmac import get_mac_address
 import voluptuous as vol
 
 from tenacity import RetryError
@@ -20,12 +16,10 @@ from homeassistant.components.media_player import (
     MediaPlayerEntityFeature,
     MediaPlayerState,
 )
-from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT, CONF_TYPE
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT, CONF_TYPE, CONF_UNIQUE_ID
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import PlatformNotReady
-from homeassistant.helpers import config_validation as cv, entity_platform
+from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 _LOGGER = logging.getLogger(__name__)
@@ -52,17 +46,6 @@ CONF_INVERSE_SPEAKER_MODE = "inverse_speaker_mode"
 CONF_SUPPORTS_ON = "supports_on"
 CONF_STANDBY_TIME = "standby_time"
 
-SERVICE_MODE = "set_mode"
-SERVICE_DESK_DB = "set_desk_db"
-SERVICE_WALL_DB = "set_wall_db"
-SERVICE_TREBLE_DB = "set_treble_db"
-SERVICE_HIGH_HZ = "set_high_hz"
-SERVICE_LOW_HZ = "set_low_hz"
-SERVICE_SUB_DB = "set_sub_db"
-SERVICE_UPDATE_DSP = "update_dsp"
-
-DSP_SCAN_INTERVAL = timedelta(seconds=3600)
-
 PLATFORM_SCHEMA = MEDIA_PLAYER_PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_HOST): cv.string,
@@ -79,21 +62,9 @@ PLATFORM_SCHEMA = MEDIA_PLAYER_PLATFORM_SCHEMA.extend(
         ): cv.boolean,
         vol.Optional(CONF_SUPPORTS_ON, default=DEFAULT_SUPPORTS_ON): cv.boolean,
         vol.Optional(CONF_STANDBY_TIME): vol.In([20, 60]),
+        vol.Optional(CONF_UNIQUE_ID): cv.string,
     }
 )
-
-
-def get_ip_mode(host):
-    """Get the 'mode' used to retrieve the MAC address."""
-    try:
-        ip_address = ipaddress.ip_address(host)
-    except ValueError:
-        return "hostname"
-
-    if ip_address.version == 6:
-        return "ip6"
-    return "ip"
-
 
 async def async_setup_platform(
     hass: HomeAssistant,
@@ -118,6 +89,15 @@ async def async_setup_platform(
 
     sources = SOURCES[speaker_type]
 
+    configured_unique_id = config.get(CONF_UNIQUE_ID)
+    if configured_unique_id:
+        unique_id = configured_unique_id
+    else:
+        # Build a stable fallback ID based on type + host + port
+        safe_host = host.replace(":", "_").replace(".", "_")
+        safe_type = speaker_type.lower()
+        unique_id = f"kef-{safe_type}-{safe_host}-{port}"
+
     _LOGGER.debug(
         "Setting up %s with host: %s, port: %s, name: %s, sources: %s",
         DOMAIN,
@@ -126,13 +106,6 @@ async def async_setup_platform(
         name,
         sources,
     )
-
-    mode = get_ip_mode(host)
-    mac = await hass.async_add_executor_job(partial(get_mac_address, **{mode: host}))
-    if mac is None or mac == "00:00:00:00:00:00":
-        raise PlatformNotReady("Cannot get the ip address of kef speaker.")
-
-    unique_id = f"kef-{mac}"
 
     media_player = KefMediaPlayer(
         name,
@@ -155,42 +128,6 @@ async def async_setup_platform(
     else:
         hass.data[DOMAIN][host] = media_player
         async_add_entities([media_player], update_before_add=True)
-
-    platform = entity_platform.async_get_current_platform()
-
-    platform.async_register_entity_service(
-        SERVICE_MODE,
-        {
-            vol.Optional("desk_mode"): cv.boolean,
-            vol.Optional("wall_mode"): cv.boolean,
-            vol.Optional("phase_correction"): cv.boolean,
-            vol.Optional("high_pass"): cv.boolean,
-            vol.Optional("sub_polarity"): vol.In(["-", "+"]),
-            vol.Optional("bass_extension"): vol.In(["Less", "Standard", "Extra"]),
-        },
-        "set_mode",
-    )
-    platform.async_register_entity_service(SERVICE_UPDATE_DSP, None, "update_dsp")
-
-    def add_service(name, which, option):
-        options = DSP_OPTION_MAPPING[which]
-        dtype = type(options[0])  # int or float
-        platform.async_register_entity_service(
-            name,
-            {
-                vol.Required(option): vol.All(
-                    vol.Coerce(float), vol.Coerce(dtype), vol.In(options)
-                )
-            },
-            f"set_{which}",
-        )
-
-    add_service(SERVICE_DESK_DB, "desk_db", "db_value")
-    add_service(SERVICE_WALL_DB, "wall_db", "db_value")
-    add_service(SERVICE_TREBLE_DB, "treble_db", "db_value")
-    add_service(SERVICE_HIGH_HZ, "high_hz", "hz_value")
-    add_service(SERVICE_LOW_HZ, "low_hz", "hz_value")
-    add_service(SERVICE_SUB_DB, "sub_db", "db_value")
 
 
 class KefMediaPlayer(MediaPlayerEntity):
@@ -232,8 +169,6 @@ class KefMediaPlayer(MediaPlayerEntity):
         self._speaker_type = speaker_type
 
         self._attr_available = False
-        self._dsp = None
-        self._update_dsp_task_remover = None
 
         self._play_state: str | None = None
 
@@ -362,23 +297,6 @@ class KefMediaPlayer(MediaPlayerEntity):
         """Send next track command."""
         await self._speaker.next_track()
 
-    async def update_dsp(self, _=None) -> None:
-        """Update the DSP settings."""
-        if self._speaker_type == "LS50" and self.state == MediaPlayerState.OFF:
-            # The LSX is able to respond when off the LS50 has to be on.
-            return
-
-        mode = await self._speaker.get_mode()
-        self._dsp = {
-            "desk_db": await self._speaker.get_desk_db(),
-            "wall_db": await self._speaker.get_wall_db(),
-            "treble_db": await self._speaker.get_treble_db(),
-            "high_hz": await self._speaker.get_high_hz(),
-            "low_hz": await self._speaker.get_low_hz(),
-            "sub_db": await self._speaker.get_sub_db(),
-            **mode._asdict(),
-        }
-
     @property
     def extra_state_attributes(self):
         """Return extra info about the KEF device."""
@@ -386,53 +304,3 @@ class KefMediaPlayer(MediaPlayerEntity):
         if self._play_state is not None:
             attrs["play_state"] = self._play_state
         return attrs
-
-    async def set_mode(
-        self,
-        desk_mode=None,
-        wall_mode=None,
-        phase_correction=None,
-        high_pass=None,
-        sub_polarity=None,
-        bass_extension=None,
-    ):
-        """Set the speaker mode."""
-        await self._speaker.set_mode(
-            desk_mode=desk_mode,
-            wall_mode=wall_mode,
-            phase_correction=phase_correction,
-            high_pass=high_pass,
-            sub_polarity=sub_polarity,
-            bass_extension=bass_extension,
-        )
-        self._dsp = None
-
-    async def set_desk_db(self, db_value):
-        """Set desk_db of the KEF speakers."""
-        await self._speaker.set_desk_db(db_value)
-        self._dsp = None
-
-    async def set_wall_db(self, db_value):
-        """Set wall_db of the KEF speakers."""
-        await self._speaker.set_wall_db(db_value)
-        self._dsp = None
-
-    async def set_treble_db(self, db_value):
-        """Set treble_db of the KEF speakers."""
-        await self._speaker.set_treble_db(db_value)
-        self._dsp = None
-
-    async def set_high_hz(self, hz_value):
-        """Set high_hz of the KEF speakers."""
-        await self._speaker.set_high_hz(hz_value)
-        self._dsp = None
-
-    async def set_low_hz(self, hz_value):
-        """Set low_hz of the KEF speakers."""
-        await self._speaker.set_low_hz(hz_value)
-        self._dsp = None
-
-    async def set_sub_db(self, db_value):
-        """Set sub_db of the KEF speakers."""
-        await self._speaker.set_sub_db(db_value)
-        self._dsp = None
